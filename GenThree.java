0bb0e7b6bf0bf4eb8428edefc7ab666cc801b712
DanielRachamim <154252101+DanielRachamim@users.noreply.github.com>
2023-12-19 19:44:00 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,22 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+	//turning a and b into int from string – this is the range given by the user 
+	int minRange = Integer.parseInt(args[0]);
+	int maxRange = Integer.parseInt(args[1]);
+	//generating the numbers in the given range
+	double rand1 = Math.random()*(maxRange-minRange+1)+minRange;
+	double rand2 = Math.random()*(maxRange-minRange+1)+minRange;
+	double rand3 = Math.random()*(maxRange-minRange+1)+minRange;
+	//printing each number as an integer
+	System.out.println((int)rand1);
+	System.out.println((int)rand2);
+	System.out.println((int)rand3);
+	//comparing between the first number and the second one to get the min 
+	int min1 = ((int)Math.min(rand1, rand2));
+	//Getting the minimum value that was generated (as an integer
+	int min2 = ((int)Math.min(min1, rand3));
+	//Printing the minimal generated number
+	System.out.println("The minimal generated number was " + min2);	
 	}
 }
